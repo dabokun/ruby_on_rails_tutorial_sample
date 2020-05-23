7ad5a532c7bdfbe8fd0dc72b2d4d085a9d8e3eac
Rikuo Uchiki <dbkn_rikuo@keio.jp>
2020-05-24 01:11:14 +0900

    Finish user signup

--- dbupdate.sh
+++ dbupdate.sh
@@ -1,3 +1,7 @@
 mv /var/rails_db/test.sqlite3 /var/rails_db/tmp.sqlite3
 cp -p /var/rails_db/tmp.sqlite3 /var/rails_db/test.sqlite3
 rm /var/rails_db/tmp.sqlite3
+
+mv /var/rails_db/development.sqlite3 /var/rails_db/tmp2.sqlite3
+cp -p /var/rails_db/tmp2.sqlite3 /var/rails_db/development.sqlite3
+rm /var/rails_db/tmp2.sqlite3
