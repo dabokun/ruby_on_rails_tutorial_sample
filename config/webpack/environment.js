d9fa2d9632828160c36999242d32017faa56a5aa
Rikuo Uchiki <dbkn_rikuo@keio.jp>
2020-05-31 18:48:07 +0900

    try to resolve heroku deploying

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,10 @@
 const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
+/*const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loader/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
-environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }
+environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }*/
 
 const webpack = require('webpack')
 environment.plugins.prepend(
